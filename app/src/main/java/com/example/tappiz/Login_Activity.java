e9a4709c04f69d8206d8a439d487f12c6fca4cec
hrebolledom <112512408+hrebolledom@users.noreply.github.com>
2022-11-15 18:06:39 -0300

    Version 1.0

---
+++ app/src/main/java/com/example/tappiz/Login_Activity.java
@@ -0,0 +1,21 @@
+package com.example.tappiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Login_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void Login (View view){
+        Intent Login = new Intent(this, Principal_Activity.class);
+        startActivity(Login);
+    }
+}
