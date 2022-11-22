f2c1a60ba8e31100afed085ac68e085b1c821784
hrebolledom <112512408+hrebolledom@users.noreply.github.com>
2022-11-22 16:08:48 -0300

    Version 1.2 (Registro de Usuario)

--- app/src/main/java/com/example/tappiz/Login_Activity.java
+++ app/src/main/java/com/example/tappiz/Login_Activity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class Login_Activity extends AppCompatActivity {
 
@@ -12,8 +13,16 @@ public class Login_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-    }
 
+        Button button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Login_Activity.this, Registro_Activity.class);
+                startActivity(intent);
+            }
+        });
+    }
     public void Login (View view){
         Intent Login = new Intent(this, Principal_Activity.class);
         startActivity(Login);
---
+++ app/src/main/java/com/example/tappiz/Registro_Activity.java
@@ -0,0 +1,14 @@
+package com.example.tappiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Registro_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro);
+    }
+}
