128c5e3d9a7c14d0bf3b7558ab4ba4bb7304f9fc
hrebolledom <112512408+hrebolledom@users.noreply.github.com>
2022-11-29 18:47:46 -0300

    Version 1.3 (Firebase)

--- app/src/main/java/com/example/tappiz/Login_Activity.java
+++ app/src/main/java/com/example/tappiz/Login_Activity.java
@@ -1,22 +1,73 @@
 package com.example.tappiz;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 
 public class Login_Activity extends AppCompatActivity {
 
     Button button2;
+    Button btn_ingresar;
+    EditText et_Email, et_password;
+    FirebaseAuth mAuth;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
+
+        et_Email = findViewById(R.id.et_Email);
+        et_password = findViewById(R.id.et_password);
+        btn_ingresar = findViewById(R.id.btn_ingresar);
+
+        btn_ingresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String emailUser = et_Email.getText().toString().trim();
+                String passUser = et_password.getText().toString().trim();
+
+                if (emailUser.isEmpty() && passUser.isEmpty()){
+                    Toast.makeText(Login_Activity.this, "Nop! Así no... Ingresa tus Datos", Toast.LENGTH_SHORT).show();
+                }else{
+                    loginUser(emailUser, passUser);
+                }
+            }
+
+            private void loginUser(String emailUser, String passUser) {
+                mAuth.signInWithEmailAndPassword(emailUser, passUser).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            finish();
+                            startActivity(new Intent(Login_Activity.this, Principal_Activity.class));
+                            Toast.makeText(Login_Activity.this, "Bienvenido", Toast.LENGTH_SHORT).show();
+                        }else{
+                            Toast.makeText(Login_Activity.this, "Error", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(Login_Activity.this, "Error al iniciar sesión", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
 
         button2 = findViewById(R.id.button2);
         button2.setOnClickListener(new View.OnClickListener() {
@@ -28,10 +79,6 @@ public class Login_Activity extends AppCompatActivity {
             }
         });
 
-    }
-    public void Login (View view){
-        Intent Login = new Intent(this, Principal_Activity.class);
-        startActivity(Login);
     }
 
     public void ubicacion (View view){
--- app/src/main/java/com/example/tappiz/MainActivity.java
+++ app/src/main/java/com/example/tappiz/MainActivity.java
@@ -6,6 +6,9 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -15,6 +18,9 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseDatabase db = FirebaseDatabase.getInstance();
+        DatabaseReference ref = db.getReference("Información");
 
         TimerTask tarea = new TimerTask() {
             @Override
