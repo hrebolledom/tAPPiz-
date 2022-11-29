2c53b427d27d500eab0d91d32a15b6ff5e7ccfb4
hrebolledom <112512408+hrebolledom@users.noreply.github.com>
2022-11-29 18:47:46 -0300

    Version 1.4 (Firebase)

--- app/src/main/java/com/example/tappiz/Principal_Activity.java
+++ app/src/main/java/com/example/tappiz/Principal_Activity.java
@@ -2,35 +2,71 @@ package com.example.tappiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.WallpaperManager;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
-import android.widget.Toast;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class Principal_Activity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+import java.io.IOException;
+
+public class Principal_Activity extends AppCompatActivity {
+
+    public int id;
+    public int numImage=0;
+    public int maxImage=5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
 
-        Spinner spinner = findViewById(R.id.spinner1);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.Box_Menu, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
-        spinner.setOnItemSelectedListener(this);
-    }
+        Button btn_aplicar=findViewById(R.id.btn_aplicar);
+        Button btn_siguiente=findViewById(R.id.btn_siguiente);
+        Button btn_anterior=findViewById(R.id.btn_anterior);
+        ImageView iv_preview=findViewById(R.id.iv_preview);
 
-    @Override
-    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-        String text = adapterView.getItemAtPosition(i).toString();
-        Toast.makeText(adapterView.getContext(),text, Toast.LENGTH_SHORT).show();
-    }
+        iv_preview.setImageResource(R.drawable.x1);
 
-    @Override
-    public void onNothingSelected(AdapterView<?> adapterView) {
+        numImage=1;
+        id=R.drawable.x1;
+
+        btn_aplicar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                WallpaperManager wp = WallpaperManager.getInstance(getApplicationContext());
+                try {
+                    wp.setResource(id);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        btn_siguiente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                numImage++;
+                if (numImage>maxImage)
+                    numImage=1;
+
+                id=getResources().getIdentifier("x"+numImage,"drawable",getPackageName());
+                iv_preview.setImageResource(id);
+            }
+        });
+
+        btn_anterior.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                numImage--;
+                if (numImage==0)
+                    numImage=6;
+
+                id=getResources().getIdentifier("x"+numImage,"drawable",getPackageName());
+                iv_preview.setImageResource(id);
+            }
+        });
 
     }
 }
